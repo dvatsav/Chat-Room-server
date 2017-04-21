f7f91b4505766a67bd2437aed6b91c81c079dcf8
Deepak Srivatsav <deepak16030@iiitd.ac.in>
2017-04-21 06:56:22 -0700

    Added chat client script

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import select
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print "Correct usage: script, IP address, port number"
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.connect((IP_address, Port))
+
+while True:
+    sockets_list = [sys.stdin, server]
+    read_sockets,write_socket, error_socket = select.select(sockets_list, [], [])
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print message
+        else:
+            message = sys.stdin.readline()
+            server.send(message)
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+server.close()
