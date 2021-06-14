2984db0d5b9638da8cd7ce06c2ca70a778a98ed3
Krishnavamsi <50523101+krishnvamsi03@users.noreply.github.com>
2021-06-14 23:41:47 +0530

    added python 3 dependency for client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
-    print "Correct usage: script, IP address, port number"
+    print("Correct usage: script, IP address, port number")
     exit()
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
